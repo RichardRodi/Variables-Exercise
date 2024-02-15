7f79b7747cc5b214af9a7a03155831122f624af7
RichardRodi <RichardmRodi@gmail.com>
2024-02-15 14:41:24 -0500

    Declared and Initialized Variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            string name = "Rich Rodi";
+            int age = 36;
+            char middleInitial = 'M';
+            bool isOver18 = true;
+            double currentTemp = 28.7;
+            decimal currentPrice = 29.99m;
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Middle Initial: {middleInitial}");
+            Console.WriteLine($"Is Over 18: {isOver18}");
+            Console.WriteLine($"Current Temp: {currentTemp}");
+            Console.WriteLine($"Current Price: {currentPrice}");
+
+            Console.WriteLine($"My name is {name} and I am {age} years old");
+
+
+
+       
+            
+;
+     }
     }
 }
